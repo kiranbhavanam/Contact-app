9689fe341f72dd79a87b119d298c48228065bcda
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-08-12 22:36:22 +0530

    Added image for each contact

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-
-const ContactCard = ({contact,removeHandler}) => {
-  const removeContact=()=>{
-    removeHandler(contact.name)
-  }
+import React from "react";
+import contactImage from './contact.png'
+const ContactCard = ({ contact, removeHandler }) => {
+  const removeContact = () => {
+    removeHandler(contact.name);
+  };
   return (
-    <div className='item  w-3/5 m-auto flex  justify-between items-center'> 
-          <div className=' content flex justify-center items-center gap-2 p-4'>
-            <div className='header'>{contact.name}</div>
-            <div>{contact.contact}</div>
-            </div>
-            <div onClick={removeContact}><i className='text-red-600 fa-solid fa-trash'></i></div>
-            </div>
-  )
-}
+    <div className="item  w-3/5 m-auto flex  justify-between items-center">
+      <div>
+      <div className=" content flex justify-center items-center gap-2 p-4 align-middle">
+      <div className="w-8 align-middle">
+        <img src={contactImage} alt="" />
+        </div>
+        <div className="header">{contact.name}</div>
+        <div>{contact.contact}</div>
+      </div>
+      </div>
+      <div onClick={removeContact}>
+        <i className="text-red-600 fa-solid fa-trash"></i>
+      </div>
+    </div>
+  );
+};
 
-export default ContactCard
+export default ContactCard;
